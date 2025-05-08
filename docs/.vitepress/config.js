e8b5497943fed658e7f8690037f0f2c0a6ed9a61
waterbanana3118 <154652860+waterbanana3118@users.noreply.github.com>
2025-05-08 14:44:29 +0800

    Update config.js

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -9,29 +9,12 @@ export default {
       { text: "滑動條", link: "/slider" }
     ],
     sidebar: [
-      {
-        text: "前言",
-        items: [
-          { text: "網站簡介", link: "/" }
-        ]
-      },
-      {
-        text: "元件",
-        items: [
-          { text: "固軸的滑動條", link: "/slider" }
-        ]
-      },
-      {
-        text: "Minecraft",
-        items: [
-          { text: "基礎設定", link: "/basics" },
-          { text: "進階設定", link: "/advanced" },
-          { text: "玩家操作", link: "/players" },
-          { text: "效果藥水", link: "/effects" },
-          { text: "生物召喚", link: "/summon" },
-          { text: "附魔道具", link: "/enchantments" }
-        ]
-      }
+      { text: "基礎設定", link: "/basics" },
+      { text: "進階設定", link: "/advanced" },
+      { text: "玩家操作", link: "/players" },
+      { text: "效果藥水", link: "/effects" },
+      { text: "生物召喚", link: "/summon" },
+      { text: "附魔道具", link: "/enchantments" }
     ],
     outline: [2, 3],
     outlineTitle: "目錄",
